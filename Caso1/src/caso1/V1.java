ac0451a4acfdbf20dd51dc293e1b0781ebead2bb
Oliver <fabrixz7@gmail.com>
2025-04-09 11:54:33 -0500

    primer commit

---
+++ Caso1/src/caso1/V1.java
@@ -0,0 +1,169 @@
+package caso1;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class V1 extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JLabel lblNewLabel;
+	private JLabel lblSegundoNmero;
+	private JLabel lblTercerNmero;
+	private JTextField txtNum1;
+	private JTextField txtNum2;
+	private JTextField txtNum3;
+	private JButton btnNewButton;
+	private JButton btnSumarTresEnteros;
+	private JButton btnSumarDosReales;
+	private JScrollPane scrollPane;
+	private JTextArea txtS;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					V1 frame = new V1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public V1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		{
+			lblNewLabel = new JLabel("Primer número");
+			lblNewLabel.setBounds(21, 26, 105, 14);
+			contentPane.add(lblNewLabel);
+		}
+		{
+			lblSegundoNmero = new JLabel("Segundo número");
+			lblSegundoNmero.setBounds(21, 59, 117, 14);
+			contentPane.add(lblSegundoNmero);
+		}
+		{
+			lblTercerNmero = new JLabel("Tercer número");
+			lblTercerNmero.setBounds(21, 95, 105, 14);
+			contentPane.add(lblTercerNmero);
+		}
+		{
+			txtNum1 = new JTextField();
+			txtNum1.setBounds(124, 23, 86, 20);
+			contentPane.add(txtNum1);
+			txtNum1.setColumns(10);
+		}
+		{
+			txtNum2 = new JTextField();
+			txtNum2.setBounds(124, 56, 86, 20);
+			contentPane.add(txtNum2);
+			txtNum2.setColumns(10);
+		}
+		{
+			txtNum3 = new JTextField();
+			txtNum3.setBounds(124, 92, 86, 20);
+			contentPane.add(txtNum3);
+			txtNum3.setColumns(10);
+		}
+		{
+			btnNewButton = new JButton("Sumar dos enteros");
+			btnNewButton.addActionListener(this);
+			btnNewButton.setBounds(237, 22, 167, 23);
+			contentPane.add(btnNewButton);
+		}
+		{
+			btnSumarTresEnteros = new JButton("Sumar tres enteros");
+			btnSumarTresEnteros.addActionListener(this);
+			btnSumarTresEnteros.setBounds(237, 55, 167, 23);
+			contentPane.add(btnSumarTresEnteros);
+		}
+		{
+			btnSumarDosReales = new JButton("Sumar dos reales");
+			btnSumarDosReales.addActionListener(this);
+			btnSumarDosReales.setBounds(237, 91, 167, 23);
+			contentPane.add(btnSumarDosReales);
+		}
+		{
+			scrollPane = new JScrollPane();
+			scrollPane.setBounds(21, 129, 386, 110);
+			contentPane.add(scrollPane);
+			{
+				txtS = new JTextArea();
+				scrollPane.setViewportView(txtS);
+			}
+		}
+	}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnSumarDosReales) {
+			do_btnSumarDosReales_actionPerformed(e);
+		}
+		if (e.getSource() == btnSumarTresEnteros) {
+			do_btnSumarTresEnteros_actionPerformed(e);
+		}
+		if (e.getSource() == btnNewButton) {
+			do_btnNewButton_actionPerformed(e);
+		}
+	}
+	protected void do_btnNewButton_actionPerformed(ActionEvent e) {
+		try {
+			int n1 = Integer.parseInt(txtNum1.getText());
+			int n2 = Integer.parseInt(txtNum2.getText());
+			Calculadora c = new Calculadora(n1, n2);
+			txtS.append("La suma es: " +c.Sumar(n1, n2));
+		} catch (Exception e2) {
+			 MostrarError();
+		}
+	}
+	private void MostrarError() {
+		JOptionPane.showMessageDialog(this,"Ingrese número(s) válido(s)");
+	}
+	
+	protected void do_btnSumarTresEnteros_actionPerformed(ActionEvent e) {
+		try {
+		int n1 = Integer.parseInt(txtNum1.getText());
+		 int n2 = Integer.parseInt(txtNum2.getText());
+		 int n3 = Integer.parseInt(txtNum3.getText());
+		 Calculadora c = new Calculadora(n1, n2, n3);
+			txtS.append("\n La suma de los tres enteros es : " +c.Sumar(n1, n2,n3));
+		} catch (Exception e2) {
+			 MostrarError();
+		}	
+	}
+	protected void do_btnSumarDosReales_actionPerformed(ActionEvent e) {
+	try {
+		double num1 = Double.parseDouble(txtNum1.getText());
+		double num2= Double.parseDouble(txtNum2.getText());
+		Calculadora c = new Calculadora(num1, num2);
+		txtS.append("\nLa suma de los reales es: " +c.Sumar(num1, num2));
+	} catch (Exception e2) {
+		MostrarError();
+	}		
+	}
+}
+
